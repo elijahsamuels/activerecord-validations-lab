4cf1f470857953c7a6bc26ce1956434b6e277930
elijahsamuels <elijahsamuels@gmail.com>
2021-01-20 20:47:19 -0700

    commit tree 5f3cf488fc727b963c2a45656c79988bc698aa31

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ActiveRecord::Base
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :phone_number, length: {minimum: 10, maximum: 10}
 end
+
+
+# All authors have a name
+# No two authors have the same name
+# Author phone numbers are exactly ten digits
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ActiveRecord::Base
+  validates :title, uniqueness: true
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i,/Guess/i]
+
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
+
+# All posts have a title
+# Post content is at least 250 characters long
+# Post summary is a maximum of 250 characters
+# Post category is either Fiction or Non-Fiction This step requires an inclusion validator, which was not outlined in the lesson. You'll need to refer to the Rails guide
+
+# Finally, add a custom validator to Post that ensures the title is sufficiently clickbait-y. If the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess", the validator should add a validation.
+
